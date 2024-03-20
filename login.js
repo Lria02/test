01f7caf5b4696a764aa40e83347819f03c1c3f11
Lria02 <clarkg860@gmail.com>
2024-03-20 12:15:54 +0800

    deploy

---
+++ login.js
@@ -0,0 +1,30 @@
+
+// 5 seconds timer
+setTimeout(function() {
+    // Hide the GIF after
+    document.getElementById("gif-container").style.display = "none";
+    
+    // Body
+    document.getElementById("main-content").style.display = "block";
+}, 5000); // 5000 milliseconds = 5 seconds
+
+
+function scrollToAboutMe() {
+    // Scroll to the About Me section
+    document.getElementById('Home').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToAboutMe() {
+    // Scroll to the About Me section
+    document.getElementById('AboutMe').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToAboutMe() {
+    // Scroll to the About Me section
+    document.getElementById('Projects').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToAboutMe() {
+    // Scroll to the About Me section
+    document.getElementById('Contacts').scrollIntoView({ behavior: 'smooth' });
+}
